b0db5c506d788c3dd03fbda544ff96ce9992c80f
Samuel Ademola <samuel.ademola43@gmail.com>
2022-07-06 22:17:08 +0100

    copy vercel api serverless to public folder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  filenameHashing: true,
+  chainWebpack: config => {
+    
+   
+    config.plugin("copy").use(CopyPlugin, [
+      {
+        patterns: [{ from: "src/api", to: "./api" }],
+      },
+    ]);
+  }
 })
