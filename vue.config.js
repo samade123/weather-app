b6a30d2d9f8cbc5b8dd2e7fd6173aa300003ae6a
Samuel Ademola <samuel.ademola43@gmail.com>
2022-09-11 17:05:09 +0100

    clean up the manifest

--- vue.config.js
+++ vue.config.js
@@ -17,16 +17,6 @@ module.exports = defineConfig({
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    // manifestOptions: {
 
-    // },
-
-    // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
-    // workboxOptions: {
-    //   // swSrc is required in InjectManifest mode.
-    //   swSrc: 'dev/sw.js',
-    //   // ...other Workbox options...
-    // }
   }
 })
