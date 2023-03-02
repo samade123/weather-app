f46e5a3767362b344ea82d9c994d821493e227a7
Samuel Ademola <samuel.ademola43@gmail.com>
2023-03-02 21:27:32 +0100

    starting to add animated layout

---
+++ src/components/WeatherSVG.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg viewBox="0 0 100 100">
+      <use xlink:href="#svg-icon" />
+    </svg>
+  </template>
+  
+  <script>
+  import svgIcon from '@/assets/svg/icon.svg';
+  
+  export default {
+    name: 'SvgImage',
+    data() {
+      return {
+        svg: svgIcon,
+      };
+    },
+  };
+  </script>
+  
