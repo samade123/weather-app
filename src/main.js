1cdeb3bafa933b51ec9798a89840a98e80f1a5bb
Samuel Ademola <samuel.ademola43@gmail.com>
2023-03-05 21:19:42 +0000

    another vue inline svg piece of code

--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import router from './router'
 import Vue3TouchEvents from "vue3-touch-events";
 import { vsSwitch } from 'vuesax3'
 
+// import Vue plugin
+import VueSvgInlinePlugin from "vue-svg-inline-plugin";
+
+// import polyfills for IE if you want to support it
+import "vue-svg-inline-plugin/src/polyfills";
+
 
 import "vuesax3/dist/vuesax.css";
-createApp(App).use(router).use(Vue3TouchEvents).use(vsSwitch).mount('#app')
+createApp(App).use(router).use(Vue3TouchEvents).use(vsSwitch).use(VueSvgInlinePlugin).mount('#app')
 
 
