e906ac21fd0a086f68c4e2a1567cbaedb0e3a197
Samuel Ademola <samuel.ademola43@gmail.com>
2023-03-20 23:45:38 +0000

    search icon functionality

--- src/components/WeatherDisplay.vue
+++ src/components/WeatherDisplay.vue
@@ -3,64 +3,64 @@
         <div class="title">the.weather</div>
         <!-- <transition name="fade-in" mode="out-in" appear appear-class=""> -->
 
-            <div class="temp-summary temp-one" v-if="!setMobile" :key="1">
-                <div class="temp">
-                    <slot name="temperature">0</slot>&#176;
-                </div>
-                <div class="location-wrapper">
-                    <div class="location">
-                        <div class="location-name">
-                            <div>
-                                <slot name="location">..</slot>
-                            </div>
-                        </div>
-                        <div class="location-date">
-                            <div>
-                                <slot name="day-and-month">..</slot>
-                                <!-- <slot name="month">..</slot> -->
-                            </div>
-                            <div>
-                                <slot name="time">..</slot>
-                            </div>
+        <div class="temp-summary temp-one" v-if="!setMobile" :key="1">
+            <div class="temp">
+                <slot name="temperature">0</slot>&#176;
+            </div>
+            <div class="location-wrapper">
+                <div class="location">
+                    <div class="location-name">
+                        <div>
+                            <slot name="location">..</slot>
                         </div>
                     </div>
-                    <div class="condition">
-                        <slot name="weather-svg">
-                            <WeatherSVG />
-                        </slot>
+                    <div class="location-date">
+                        <div>
+                            <slot name="day-and-month">..</slot>
+                            <!-- <slot name="month">..</slot> -->
+                        </div>
+                        <div>
+                            <slot name="time">..</slot>
+                        </div>
+                    </div>
+                </div>
+                <div class="condition">
+                    <slot name="weather-svg">
+                        <WeatherSVG />
+                    </slot>
+                    <div class="cloudy">
+                        <slot name="condition">Mostly Clear</slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="temp-summary temp-two" v-else :key="2">
+            <div class="location-wrapper">
+                <div class="condition">
+                    <slot name="weather-svg">
+                        <WeatherSVG />
+                    </slot>
+                </div>
+                <div class="location">
+                    <div class="location-name">
+                        <div>
+                            <slot name="location">..</slot>
+                        </div>
+                    </div>
+                    <div class="location-date">
                         <div class="cloudy">
-                            <slot name="condition">Mostly Clear</slot>
+                            <slot name="condition">Mostly Clear</slot>, <slot name="time">..</slot>
                         </div>
                     </div>
                 </div>
+
             </div>
 
-            <div class="temp-summary temp-two" v-else :key="2">
-                <div class="location-wrapper">
-                    <div class="condition">
-                        <slot name="weather-svg">
-                            <WeatherSVG />
-                        </slot>
-                    </div>
-                    <div class="location">
-                        <div class="location-name">
-                            <div>
-                                <slot name="location">..</slot>
-                            </div>
-                        </div>
-                        <div class="location-date">
-                            <div class="cloudy">
-                                <slot name="condition">Mostly Clear</slot>, <slot name="time">..</slot>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="temp">
-                    <slot name="temperature">0</slot><span>&#176;</span>
-                </div>
+            <div class="temp">
+                <slot name="temperature">0</slot><span>&#176;</span>
             </div>
+        </div>
         <!-- </transition> -->
         <div class="spacer condition" v-if="!setMobile"></div>
     </div>
@@ -68,8 +68,11 @@
 
     <div class="right new" v-if="!setMobile">
         <div class="search-container">
-            <input type="text" class="search">
-            <div class="search-icon">i</div>
+            <input type="text" class="search" ref="searchInput" @blur="playIcon">
+            <div class="search-icon" @click="playIcon">
+                <Vue3Lottie :animationData="searchIconJson" height="100%" width="100%" ref="icon" :loop="false"
+                    :pauseAnimation="false" :autoPlay="false" />
+            </div>
         </div>
         <div class="search-results-container">
             <div class="result">Birmingham</div>
@@ -79,9 +82,15 @@
         </div>
         <div class="weather-details-title">Weather Details</div>
         <div class="weather-details-container">
-            <div class="stats">Cloudy <span><slot name="cloudy">0</slot></span></div>
-            <div class="stats">Humidity <span><slot name="humidity">0</slot></span></div>
-            <div class="stats">Wind <span><slot name="wind">0</slot></span></div>
+            <div class="stats">Cloudy <span>
+                    <slot name="cloudy">0</slot>
+                </span></div>
+            <div class="stats">Humidity <span>
+                    <slot name="humidity">0</slot>
+                </span></div>
+            <div class="stats">Wind <span>
+                    <slot name="wind">0</slot>
+                </span></div>
         </div>
         <div class="weather-details-title">Next 3 Hours</div>
         <div class="next-seven-container">
@@ -93,6 +102,8 @@
 <script>
 import WeatherSVG from "@/components/WeatherSVG.vue";
 import { widthFunction } from "@/composables/Mobile.js";
+import searchIconJson from "@/assets/lottie-files/search.json";
+import { ref } from 'vue';
 
 export default {
     name: 'WeatherDisplay',
@@ -101,8 +112,22 @@ export default {
     },
     setup(props, ctx) {
         const { width, setMobile, getScreenCategory } = widthFunction();
+        const icon = ref(null) //lottie player element
+        const searchInput = ref(null) //input element
+        const iconPlayState = ref(false)
+
+        const playIcon = () => {
+            iconPlayState.value = !iconPlayState.value
+            if (iconPlayState.value) {
+                icon.value.playSegments([0, 50], true)
+                searchInput.value.focus()
+            } else {
+                icon.value.playSegments([50, 0], true)
+            }
+        }
+
         return {
-            width, setMobile, getScreenCategory,
+            width, setMobile, getScreenCategory, searchIconJson, icon, playIcon, searchInput,
         };
 
     },
@@ -113,6 +138,5 @@ export default {
 @use "./../stylesheets/dashboard/theme-old.scss" as *;
 @use "./../stylesheets/dashboard/theme-new.scss" as *;
 @use "./../stylesheets/transitions.scss" as *;
-
 </style>
   
--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 import Vue3TouchEvents from "vue3-touch-events";
-import { vsSwitch } from 'vuesax3'
+import { vsSwitch } from "vuesax3";
 
 // import Vue plugin
 import VueSvgInlinePlugin from "vue-svg-inline-plugin";
 
 // import polyfills for IE if you want to support it
 import "vue-svg-inline-plugin/src/polyfills";
+import Vue3Lottie from 'vue3-lottie'
+import 'vue3-lottie/dist/style.css'
 
 
 import "vuesax3/dist/vuesax.css";
-createApp(App).use(router).use(Vue3TouchEvents).use(vsSwitch).use(VueSvgInlinePlugin).mount('#app')
-
-
+createApp(App)
+  .use(router)
+  .use(Vue3TouchEvents)
+  .use(vsSwitch)
+  .use(VueSvgInlinePlugin)
+  .use(Vue3Lottie)
+  .mount("#app");
