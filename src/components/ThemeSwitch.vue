736799d671084c16ca241a2122ebe3f4e6a189b2
Samuel Ademola <samuel.ademola43@gmail.com>
2023-03-20 07:38:30 +0000

    added dissapear functionality to theme switcher

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -1,31 +1,15 @@
 <template>
   <transition name="fade" appear>
-    <div class="theme-body">
-      <fieldset
-        class="theme-switcher"
-        role="radiogroup"
-        aria-labelledby="theme-switcher-label"
-      >
+    <div class="theme-body" :class="{ 'dissapear': dissapear }">
+      <div class="minimise" @click="dissapear = true">-</div>
+      <fieldset class="theme-switcher" role="radiogroup" ref="draggable" aria-labelledby="theme-switcher-label">
         <legend id="theme-switcher-label">Choose a theme:</legend>
         <div class="theme-option">
-          <input
-            type="radio"
-            id="theme-old"
-            name="theme"
-            value="light"
-            checked
-            aria-labelledby="theme-old-label"
-          />
+          <input type="radio" id="theme-old" name="theme" value="light" checked aria-labelledby="theme-old-label" />
           <label for="theme-old" id="theme-old-label">Old theme</label>
         </div>
         <div class="theme-option">
-          <input
-            type="radio"
-            id="theme-new"
-            name="theme"
-            value="new"
-            aria-labelledby="new-theme-label"
-          />
+          <input type="radio" id="theme-new" name="theme" value="new" aria-labelledby="new-theme-label" />
           <label for="theme-new" id="new-theme-label">New theme</label>
         </div>
       </fieldset>
@@ -34,11 +18,38 @@
 </template>
 
 <script>
-import { onBeforeMount, ref } from "@vue/runtime-core";
+import { onMounted, ref } from "@vue/runtime-core";
 export default {
   name: "ThemeSwitch",
   setup(props, ctx) {
-    return {};
+    const draggable = ref(null);
+    const dissapear = ref(false);
+
+    onMounted(() => {
+      draggable.value.addEventListener("mousedown", onMouseDown);
+    });
+
+    const onMouseDown = (event) => {
+      event.preventDefault();
+      document.addEventListener("mousemove", onMouseMove);
+      document.addEventListener("mouseup", onMouseUp);
+    };
+
+    const onMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      draggable.value.style.left = `${clientX}px`;
+      draggable.value.style.top = `${clientY}px`;
+    };
+
+    const onMouseUp = () => {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+
+    return {
+      draggable,
+      dissapear,
+    };
   },
 };
 </script>
@@ -61,6 +72,26 @@ export default {
   border-radius: 20px;
 
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+  // white-space: ;
+
+  &.dissapear {
+    opacity: 0;
+    overflow: hidden;
+    pointer-events: none;
+    width: 10px;
+    height: 10px;
+  }
+
+  div.minimise {
+    opacity: 1!important;
+    position: relative;
+    top: -10px;
+    right: 0;
+    width: 20px;
+    border-radius: 50%;
+    font-weight: 700;
+    background: white;
+  }
 }
 
 :root:has(#theme-old:checked) {
@@ -91,6 +122,8 @@ legend {
   line-height: 1.5;
   color: inherit;
   border: 0;
-}
+  cursor: move;
+  /* Make the legend draggable */
 
+}
 </style>
