358f15a8b917237c25ef96fe2c95d7a1ebe4e97e
Samuel Ademola <samuel.ademola43@gmail.com>
2023-04-19 08:50:59 +0100

    package changes to remove redundant packages save space

--- src/main.js
+++ src/main.js
@@ -5,11 +5,6 @@ import router from "./router";
 import Vue3TouchEvents from "vue3-touch-events";
 import { vsSwitch } from "vuesax3";
 
-// import Vue plugin
-import VueSvgInlinePlugin from "vue-svg-inline-plugin";
-
-// import polyfills for IE if you want to support it
-import "vue-svg-inline-plugin/src/polyfills";
 import Vue3Lottie from 'vue3-lottie'
 import 'vue3-lottie/dist/style.css'
 
@@ -19,6 +14,5 @@ createApp(App)
   .use(router)
   .use(Vue3TouchEvents)
   .use(vsSwitch)
-  .use(VueSvgInlinePlugin)
   .use(Vue3Lottie)
   .mount("#app");
