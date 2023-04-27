7959991623f41f5ee2246e8c5fc3a3054b417d5b
Samuel Ademola <samuel.ademola43@gmail.com>
2023-04-27 08:07:34 +0100

    moved css to scss files

--- src/main.js
+++ src/main.js
@@ -6,10 +6,6 @@ import Vue3TouchEvents from "vue3-touch-events";
 import { vsSwitch } from "vuesax3";
 
 import Vue3Lottie from 'vue3-lottie'
-import 'vue3-lottie/dist/style.css'
-
-
-import "vuesax3/dist/vuesax.css";
 createApp(App)
   .use(router)
   .use(Vue3TouchEvents)
