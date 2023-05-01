40fc3aaac1bc3376bf9e571decbb9a9f0c84d4c1
Samuel Ademola <samuel.ademola43@gmail.com>
2023-05-01 22:58:35 +0100

    fix for coposale handling responsiveness to match css

--- src/composables/Mobile.js
+++ src/composables/Mobile.js
@@ -11,15 +11,15 @@ export function widthFunction() {
 
         let category = 'small'
 
-        width < 600 ? (category = 'medium') : (category = 'small');
-        width < 1400 ? (category = 'large') : (category = 'medium');
+        width <= 600 ? (category = 'medium') : (category = 'small');
+        width <= 1400 ? (category = 'large') : (category = 'medium');
         return category
 
     }
     watch(
         width,
         (width) => {
-            width < 600 ? (setMobile.value = true) : (setMobile.value = false);
+            width <= 600 ? (setMobile.value = true) : (setMobile.value = false);
         },
         { immediate: true }
     );
